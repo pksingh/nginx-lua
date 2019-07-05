2c6e44ddd99c777ff1638bc6406ce873c3e6b3f7
Prasanta Singh <singh.prasanta@gmail.com>
2019-07-05 11:02:56 +0530

    def GetStatus

--- microcalc/addPy/main.py
+++ microcalc/addPy/main.py
@@ -1,4 +1,5 @@
-from flask import Flask
+from flask import Flask, request
+from flask_restful import Resource, Api, abort
 
 APP_VERSION = 'v1'
 APP_BASEPATH = '/api/' + APP_VERSION
@@ -9,6 +10,12 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     return {'world': 'welcome all'}
+
+api = Api(app)
+
+class GetStatus(Resource):
+    def get(self):
+        return {'data': 'ok'}
 
 
 if __name__ == '__main__':
