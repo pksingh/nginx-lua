5a619e1d9507d4e4e48fe658943fe06f7950e1ee
Prasanta Singh <singh.prasanta@gmail.com>
2019-07-21 20:37:45 +0530

    define consts and run the server from APP_PORT

--- microcalc/subPy/main.py
+++ microcalc/subPy/main.py
@@ -1,6 +1,11 @@
 from flask import Flask, request
 
+APP_VERSION = 'v1'
+APP_BASEPATH = '/api/' + APP_VERSION
+APP_SERVICE = 'name: sub, version: ' + APP_VERSION
+APP_PORT = '8082'
+
 app = Flask(__name__)
 @app.route('/')
 def hello():
-    return {'world': 'welcome all'}
+    return {'world': 'welcome all : '+APP_SERVICE}
