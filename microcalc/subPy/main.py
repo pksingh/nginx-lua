7aa3ce8d3f8ce5307ff514c2d9d1b341d7e01a1d
Prasanta Singh <singh.prasanta@gmail.com>
2019-07-22 20:41:23 +0530

    run the server from APP_PORT

--- microcalc/subPy/main.py
+++ microcalc/subPy/main.py
@@ -9,3 +9,6 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     return {'world': 'welcome all : '+APP_SERVICE}
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=APP_PORT, debug=True)
