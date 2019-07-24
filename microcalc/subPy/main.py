032e3e4809c0c8c6ad4d252ee933203670c2532d
Prasanta Singh <singh.prasanta@gmail.com>
2019-07-24 21:43:21 +0530

    def GetStatus and Attached /status endpoint

--- microcalc/subPy/main.py
+++ microcalc/subPy/main.py
@@ -1,4 +1,5 @@
 from flask import Flask, request
+from flask_restful import Resource, Api, abort
 
 APP_VERSION = 'v1'
 APP_BASEPATH = '/api/' + APP_VERSION
@@ -10,5 +11,12 @@ app = Flask(__name__)
 def hello():
     return {'world': 'welcome all : '+APP_SERVICE}
 
+api = Api(app)
+
+class GetStatus(Resource):
+    def get(self):
+        return {'data': 'ok'}
+
+api.add_resource(GetStatus, APP_BASEPATH + '/status')
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=APP_PORT, debug=True)
