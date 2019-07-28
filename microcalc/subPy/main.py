b1774b32b76c04fa6d9fe7f2c729b3a27f0553ed
Prasanta Singh <singh.prasanta@gmail.com>
2019-07-28 09:43:37 +0530

    sending json resp with calulated value

--- microcalc/subPy/main.py
+++ microcalc/subPy/main.py
@@ -29,6 +29,15 @@ class PostSub(Resource):
         op1,op2 = ops
         print('Received {} {}'.format(op1, op2))
 
+        op1 = int(op1)
+        op2 = int(op2)
+
+        return {
+            'result': int(op1-op2),
+            'operands': [op1,op2],
+            'service': APP_SERVICE
+        }
+
 api.add_resource(GetStatus, APP_BASEPATH + '/status')
 api.add_resource(PostSub, APP_BASEPATH + '/sub')
 
