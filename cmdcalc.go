9dbf1c92f1d9588b22477ddf3dd1356b8602d00d
Prasanta Singh <singh.prasanta@gmail.com>
2019-09-15 11:21:28 +0530

    processStack() will do all processing - WIP

--- cmdcalc.go
+++ cmdcalc.go
@@ -6,12 +6,20 @@ import (
 	"os"
 )
 
+// processStack() : processes the string expression
+func processStack(e string) (float64, error) {
+	result := 0.0
+	return result, nil
+}
+
 func main() {
 	for {
 		scanner := bufio.NewScanner(os.Stdin)
 		fmt.Print("calc>")
 		for scanner.Scan() {
 			fmt.Println("exp:", scanner.Text())
+			res, _ := processStack(scanner.Text())
+			fmt.Println(res)
 			fmt.Print("calc>")
 		}
 	}
