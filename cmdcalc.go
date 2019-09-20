d3d13c9f7097dfdade9ff07bc7c6fa244c78bc94
Prasanta Singh <singh.prasanta@gmail.com>
2019-09-20 20:26:16 +0530

    Lets spit the exp string and some error check

--- cmdcalc.go
+++ cmdcalc.go
@@ -2,13 +2,22 @@ package main
 
 import (
 	"bufio"
+	"errors"
 	"fmt"
 	"os"
+	"strings"
 )
 
 // processStack() : processes the string expression
 func processStack(e string) (float64, error) {
 	result := 0.0
+	fmt.Printf("Passed Arg: %#v, Len: %d\n", e, len(e))
+
+	c := strings.Split(e, " ")
+	fmt.Println("Arg:", e, " len:", len(c))
+	if len(c)-1 < 2 {
+		return 0.0, errors.New("error: missing arguments (pass space between number and symbols)")
+	}
 	return result, nil
 }
 
