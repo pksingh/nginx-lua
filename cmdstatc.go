f08dbcf2516bc722fb937e64b160f2df4fc0f4a1
Prasanta Singh <singh.prasanta@gmail.com>
2020-01-09 22:19:07 +0530

    lets define Register - to evaluates stats

--- cmdstatc.go
+++ cmdstatc.go
@@ -22,3 +22,19 @@ type XRecord struct {
 	evalModes             bool
 }
 
+// Register a way to store evaluated results.
+type Register struct {
+	Total             float64
+	Mean              float64
+	Median            float64
+	Range             float64
+	Variance          float64
+	StandardDeviation float64
+	SortedMedian      float64
+	MaxIndices        []int
+	MaxValue          float64
+	MinIndices        []int
+	MinValue          float64
+	ModeRepeatCount   int
+	Modes             []float64
+}
