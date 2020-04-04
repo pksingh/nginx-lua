10dd3949427184747285071ec5657670a99af2d9
Prasanta Singh <singh.prasanta@gmail.com>
2020-04-04 11:13:32 +0530

    define main and  start server

--- monocalc/main.go
+++ monocalc/main.go
@@ -1,8 +1,18 @@
 package main
 
+import (
+	"log"
+	"net/http"
+)
 
 const (
 	AppVersion  = "v1"
 	AppBasepath = "/api/" + AppVersion
 	AppService  = "name: monocalc, version: " + AppVersion
 )
+
+func main() {
+	log.Println("Server Starting on 8080")
+	log.Fatal(http.ListenAndServe(":8080", http.DefaultServeMux))
+
+}
