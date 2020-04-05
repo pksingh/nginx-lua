735252d42dbcf8785b9b55fdde54381dd87de578
Prasanta Singh <singh.prasanta@gmail.com>
2020-04-05 11:32:56 +0530

    lets define /status endpoint

--- monocalc/main.go
+++ monocalc/main.go
@@ -12,6 +12,11 @@ const (
 )
 
 func main() {
+	http.HandleFunc("/status", func(rw http.ResponseWriter, req *http.Request) {
+		rw.WriteHeader(http.StatusOK)
+		rw.Write([]byte(`{"data":"ok"}`))
+	})
+
 	log.Println("Server Starting on 8080")
 	log.Fatal(http.ListenAndServe(":8080", http.DefaultServeMux))
 
