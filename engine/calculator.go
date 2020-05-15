02e894e18d4b2d37630c9045ecc5f1c2efa1552f
Prasanta Singh <singh.prasanta@gmail.com>
2020-05-15 09:59:54 +0530

    Calculate() added to process postfix expression

--- engine/calculator.go
+++ engine/calculator.go
@@ -1,5 +1,61 @@
 package engine
 
+import (
+	"fmt"
+	"log"
+	"strconv"
+)
+
+// Calculate postfix expression
+func Calculate(tokens []Token) int {
+	result := 0
+
+	stack := []int{}
+
+	for len(tokens) > 0 {
+		t, shifted := ShiftToken(tokens)
+		tokens = shifted
+
+		if t.Type == NUM {
+			intValue, err := strconv.Atoi(t.Value)
+			if err != nil {
+				log.Fatalf("cast error %s", t.Value)
+			}
+			stack = append(stack, intValue)
+		} else {
+			// Otherwise we got OPR, get operands
+			l, lPoped := popInt(stack)
+			stack = lPoped
+
+			r, rPoped := popInt(stack)
+			stack = rPoped
+
+			var calc int
+			switch t.Value {
+			case "+":
+				calc = r + l
+				stack = append(stack, calc)
+			case "-":
+				calc = r - l
+				stack = append(stack, calc)
+			case "*":
+				calc = r * l
+				stack = append(stack, calc)
+			case "/":
+				calc = r / l
+				stack = append(stack, calc)
+			case "%":
+				calc = r % l
+				stack = append(stack, calc)
+			}
+			fmt.Printf("Evaluate: %d %s %d = %d\n", r, t.Value, l, calc)
+		}
+	}
+
+	result, _ = popInt(stack)
+
+	return result
+}
 
 func popInt(values []int) (int, []int) {
 	var v int
