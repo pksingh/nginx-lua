59bb34002f173d9396111576445e35b4129f44f0
Prasanta Singh <singh.prasanta@gmail.com>
2021-03-05 21:01:31 +0530

    set some const for mutiple service & GetEnvOrDef()

---
+++ microcalc/calcparser/configs.go
@@ -0,0 +1,23 @@
+package main
+
+var (
+	ADD_HOST = GetEnvOrDefault("ADD_HOST", "localhost")
+	ADD_PORT = GetEnvOrDefault("ADD_PORT", "8081")
+	ADD_URI  = GetEnvOrDefault("ADD_URI", "/api/v1/add")
+
+	SUB_HOST = GetEnvOrDefault("SUB_HOST", "localhost")
+	SUB_PORT = GetEnvOrDefault("SUB_PORT", "8082")
+	SUB_URI  = GetEnvOrDefault("SUB_URI", "/api/v1/sub")
+
+
+	APP_VERSION = "v1"
+	APP_SERVICE = "name: calcparser, version: " + APP_VERSION
+)
+
+func GetEnvOrDefault(name string, def string) string {
+	name, ok := os.LookupEnv(name)
+	if !ok {
+		return def
+	}
+	return name
+}
