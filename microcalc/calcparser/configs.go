a0172b8db30e107bc0f450c8c608e6011785bd7c
Prasanta Singh <singh.prasanta@gmail.com>
2021-03-06 19:05:08 +0530

    define ResulrResponse

--- microcalc/calcparser/configs.go
+++ microcalc/calcparser/configs.go
@@ -14,6 +14,14 @@ var (
 	APP_SERVICE = "name: calcparser, version: " + APP_VERSION
 )
 
+// ResultResponse is the Final Response served
+type ResultResponse struct {
+	Input   string        `json:"input" binding:"required"`
+	Origins []interface{} `json:"origins,omitempty" binding:"required"`
+	// Operands         `json:"operands" binding:"required"`
+	Result  int    `json:"result" binding:"required"`
+	Service string `json:"service" binding:"required"`
+}
 func GetEnvOrDefault(name string, def string) string {
 	name, ok := os.LookupEnv(name)
 	if !ok {
