18f58780b7c87662f4724756e09cbe2b591f0c9f
Prasanta Singh <singh.prasanta@gmail.com>
2021-03-12 19:37:17 +0530

    def Evaluate() - WIP

--- microcalc/calcparser/evaluator.go
+++ microcalc/calcparser/evaluator.go
@@ -1,5 +1,19 @@
 package main
 
+import (
+	. "engine" // will refer to github.com/pksingh/uservices/engine
+)
+
+// Evaluate postfix expression
+func Evaluate(tokens []Token) ResultResponse {
+	result := ResultResponse{}
+
+	stack := []int{}
+
+	result.Result, _ = popInt(stack)
+
+	return result
+}
 
 func popInt(values []int) (int, []int) {
 	var v int
