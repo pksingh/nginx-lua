1f13572d876f8919962045368bbb71a81a4ab6f4
Prasanta Singh <singh.prasanta@gmail.com>
2021-03-14 20:11:11 +0530

    def Evaluate() - implemented

--- microcalc/calcparser/evaluator.go
+++ microcalc/calcparser/evaluator.go
@@ -1,6 +1,10 @@
 package main
 
 import (
+	"fmt"
+	"log"
+	"strconv"
+
 	. "engine" // will refer to github.com/pksingh/uservices/engine
 )
 
@@ -9,6 +13,32 @@ func Evaluate(tokens []Token) ResultResponse {
 	result := ResultResponse{}
 
 	stack := []int{}
+
+	for len(tokens) > 0 {
+		t, shifted := ShiftToken(tokens)
+		tokens = shifted
+
+		if t.Type == NUM {
+			intValue, err := strconv.Atoi(t.Value)
+			if err != nil {
+				log.Fatalf("cast error %s", t.Value)
+			}
+			stack = append(stack, intValue)
+		} else {
+			// Otherwise we got OPR, get operands
+			l, lPoped := popInt(stack)
+			stack = lPoped
+
+			r, rPoped := popInt(stack)
+			stack = rPoped
+			nr := NodeResponse{}
+			fmt.Println("res : ", nr)
+			calc := nr.Result
+			result.Origins = append(result.Origins, nr)
+			stack = append(stack, calc)
+			fmt.Printf("Evaluate: %d %s %d = %d\n", r, t.Value, l, calc)
+		}
+	}
 
 	result.Result, _ = popInt(stack)
 
