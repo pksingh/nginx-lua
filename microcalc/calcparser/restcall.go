9fc966feb2ac1f7b65cdc134a96532709ff6a8ef
Prasanta Singh <singh.prasanta@gmail.com>
2021-03-20 20:49:40 +0530

    error handling in DoPost()

--- microcalc/calcparser/restcall.go
+++ microcalc/calcparser/restcall.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"fmt"
 	"net/http"
+	"os"
 	"time"
 )
 
@@ -11,12 +12,21 @@ func DoPost(operator string, opr, opl int) (*http.Response, error) {
 	requestURL := NodeEndpoint[operator]
 	jsonBody := fmt.Sprintf(`{ "operands": [ %d, %d ] }`, opr, opl)
 	bodyReader := bytes.NewReader([]byte(jsonBody))
-	req, _ := http.NewRequest(http.MethodPost, requestURL, bodyReader)
+	req, err := http.NewRequest(http.MethodPost, requestURL, bodyReader)
+	if err != nil {
+		fmt.Printf("client: could not create request: %s\n", err)
+		os.Exit(1)
+	}
 	req.Header.Set("Content-Type", "application/json")
+
 	client := http.Client{
 		Timeout: 30 * time.Second,
 	}
 
-	res, _ := client.Do(req)
+	res, err := client.Do(req)
+	if err != nil {
+		fmt.Printf("client: error making http request: %s\n", err)
+		os.Exit(1)
+	}
 	return res, nil
 }
