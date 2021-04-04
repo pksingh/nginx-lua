ca1355781528c96b0cedb56d0086c5b6d8a38f51
Prasanta Singh <singh.prasanta@gmail.com>
2021-04-04 21:21:34 +0530

    define consts and run the server from APP_PORT

--- microcalc/mulJs/app.js
+++ microcalc/mulJs/app.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const APP_VERSION = 'v1';
+const APP_BASEPATH = `/api/${APP_VERSION}`;
+const APP_SERVICE = `name: mul, version: ${APP_VERSION}`;
+const APP_PORT = process.env.APP_PORT || 8083;
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-app.listen(port, () => {
-  console.log(`service launched at  ${port}`);
+app.listen(APP_PORT, () => {
+  console.log(`service ${APP_SERVICE} launched at  ${APP_PORT}`);
 });
