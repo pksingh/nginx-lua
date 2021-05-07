93a07282fea457cfda40413b37da9ba71d9410b3
Prasanta Singh <singh.prasanta@gmail.com>
2021-05-07 21:09:01 +0530

    define the APP constants & run APP PORT

--- microcalc/divJs/app.js
+++ microcalc/divJs/app.js
@@ -1,10 +1,21 @@
 const Koa = require("koa");             // import koa
 const logger = require("koa-logger");   // import koa logger
-const app = new Koa();
 
+// define the APP constants
+const APP_VERSION = 'v1';
+const APP_BASEPATH = `/api/${APP_VERSION}`;
+const APP_SERVICE = `name: div, version: ${APP_VERSION}`;
+const APP_PORT = process.env.APP_PORT || 8084;
+
+//create the koa application
+const app = new Koa();
 app.use(logger());                      //Enable logger
+
 app.use(async ctx => {
   ctx.body = 'Hello World';
 });
 
-app.listen(4000, () => console.log("Server Listening on Port 4000"));
+// Turn on the server
+app.listen(APP_PORT, () => {
+  console.log(`service ${APP_SERVICE} launched at  ${APP_PORT}`);
+});
